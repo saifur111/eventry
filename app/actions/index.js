764cf0868d6255f170fbaccb9e804b9bc62ed3c8
saifur111 <ssaaiiffuurr240@gmail.com>
2024-04-25 12:04:27 +0600

    complete interest event  section

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,8 +1,8 @@
 "use server";
 
-import { createUser, findUserByCredentials } from "@/db/queries";
+import { revalidatePath } from 'next/cache'
+import { createUser, findUserByCredentials, updateInterest } from "@/db/queries";
 import { redirect } from "next/navigation";
-
 
 async function registerUser(formData) {
     const user = Object.fromEntries(formData);
@@ -22,4 +22,13 @@ async function performLogin(formData) {
     }
 }
 
-export { registerUser, performLogin };
+async function addInterestedEvent(eventId, authId) {
+    try {
+        await updateInterest(eventId, authId);
+    } catch(error) {
+        throw error;
+    }
+    revalidatePath('/');
+}
+
+export { registerUser, performLogin, addInterestedEvent };
--- db/queries.js
+++ db/queries.js
@@ -2,6 +2,7 @@ import { eventModel } from "@/models/event-models";
 import { userModel } from "@/models/user-model";
 
 import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/utils/data-util";
+import mongoose from "mongoose";
 
 // get all events from mongodb database
 async function getAllEvents() {
@@ -28,10 +29,29 @@ async function findUserByCredentials(credentials) {
     }
     return null;
 }
+// update event interest
+async function updateInterest(eventId, authId) {
 
+    const event = await eventModel.findById(eventId);
+
+    if (event) {
+        const foundUsers = event.interested_ids.find(id => id.toString() === authId);
+
+        if(foundUsers) {
+            event.interested_ids.pull(new mongoose.Types.ObjectId(authId));
+        } else {
+            event.interested_ids.push(new mongoose.Types.ObjectId(authId));
+        }
+
+        event.save();
+    }
+
+
+}
 export {
     getAllEvents,
     getEventById,
     createUser,
-    findUserByCredentials
+    findUserByCredentials,
+    updateInterest
 }
